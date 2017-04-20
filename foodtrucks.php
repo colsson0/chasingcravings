025ab45afa8eb8cdd552cd2983c3a2785226136a
NickBurnette <burnette.nick@gmail.com>
2017-04-20 01:07:02 -0400

    Update foodtrucks.php

--- foodtrucks.php
+++ foodtrucks.php
@@ -1,4 +1,9 @@
 <!DOCTYPE html>
+<!--	Author: Chris Olsson
+		Date:	March 25, 2017
+		File:	foodtrucks.php
+		Purpose: Displays the trucks that are contained in the database
+-->
 <html>
 <head>
 	<?php include "headInfo.php"?>
@@ -20,6 +25,8 @@
 				}
 				// Generate the query to get all of the needed food truck data
 				$userQuery = "SELECT pictureURL, truckID, truckName, (SELECT avg(rating) FROM comments WHERE trucks.truckID = comments.truckID) AS rating FROM trucks;";
+				//Found 2 versions on this. If you want to change it to the below option let me know and ill redo the css. -Nick
+				//$userQuery = "SELECT truckID, truckName, (SELECT avg(rating) FROM comments WHERE trucks.truckID = comments.truckID) AS rating FROM trucks;";
 				$result = mysqli_query($connect, $userQuery);
 
 				if (!$result) 
@@ -36,20 +43,27 @@
 				else 
 				{ 
 					print("<h1>Food Trucks</h1>");
-					//print("<div class=\"form-wrapper\">");
+					print("<div id=\"food-trucks-wrapper\">");
 					print("<table border = \"1\">");
-					print("<tr><th></th><th>Truck Name</th>
-						<th>Rating</th></tr>");
+					//print("<tr><th></th><th>Truck Name</th>
+						//<th>Rating</th></tr>");
 					while ($row = mysqli_fetch_assoc($result))
 					{
-						print ("<tr><td><img src=".$row['pictureURL']."></td><td><a href=\"truck.php?truckID=".$row['truckID']."\">".$row['truckName']."</a></td><td>"
-								.number_format($row['rating'], 2).
-								"</td></tr>");
+						print ("<div class=\"food-truck\"><div class=\"img\"><img src=".$row['pictureURL']."></div><div id=\"food-truck-info-wrapper\"><div class=\"name\"><a href=\"truck.php?truckID=".$row['truckID']."\">".$row['truckName']."</a></div><div class=\"rating\">".number_format($row['rating'], 2)."</div></div></div>");
+						//table style. kept to compare to other option.
+						//print ("<tr><td><img src=".$row['pictureURL']."></td><td><a href=\"truck.php?truckID=".$row['truckID']."\">".$row['truckName']."</a></td><td>"
+						
+						//Found 2 versions on this. If you want to change it to the below option let me know and ill redo the css. -Nick
+						//print ("<tr><td>".$row['truckID']."</td><td>".$row['truckName']."</td><td>"
+						
+								//.number_format($row['rating'], 2).
+								//"</td></tr>");
 					}
 					print("</table");
 				}
 				mysqli_close($connect);   // close the connection
 			?>
+			</div>
 		</div>
 	</div>
 </body>
